6529d9b1735e0a6a50b32c88c9ec35498018ca94
Ela Rozmus <rozmus.ela@gmail.com>
2023-01-04 13:40:15 +0100

    do wyboru 3 zwierzeta

--- losowe_zdj_3_pies.py
+++ losowe_zdj_3_pies.py
@@ -3,50 +3,63 @@
 import requests
 import json
 import webbrowser
-
-howManyPictures =int(input("How many pictures you want to see?: "))
-parameter = {
-    "count" : howManyPictures
+from enum import IntEnum
+def next_level(howManyPictures):
     
-    }
-urlDict = {"urlShiba" : "http://shibe.online/api/shibes",
-           "urlCats" : "http://shibe.online/api/cats",
-           "urlBirds" : "http://shibe.online/api/birds"}
+    parameter = {
+        "count" : howManyPictures
+        
+        }
+    urlDict = {"urlShiba" : "http://shibe.online/api/shibes",
+               "urlCats" : "http://shibe.online/api/cats",
+               "urlBirds" : "http://shibe.online/api/birds"}
+    
+    animalReguestDict = {"rshiba" : requests.get(urlDict["urlShiba"], parameter),
+                     "rCats" : requests.get(urlDict["urlCats"], parameter),
+                     "rBirds" : requests.get(urlDict["urlBirds"], parameter)}
+    
+    
+    #SHIBA
+    try:
+        pictureShiba = animalReguestDict["rshiba"].json()
+    except json.decoder.JSONDecodeError():
+        print("niepoprawny format")
+    else:
+        for dog in pictureShiba:
+           webbrowser.open_new_tab(urlDict["urlShiba"])
+    #Cats
+    try:
+        pictureCats = animalReguestDict["rCats"].json()
+    except json.decoder.JSONDecodeError():
+        print("niepoprawny format")
+    else:
+        for dog in pictureCats:
+           webbrowser.open_new_tab(urlDict["urlCats"]) 
+    #birds
+    try:
+        pictureBirds = animalReguestDict["rBirds"].json()
+    except json.decoder.JSONDecodeError():
+        print("niepoprawny format")
+    else:
+        for dog in pictureBirds:
+           webbrowser.open_new_tab(urlDict["urlBirds"])
 
-animalReguestDict = {"rshiba" : requests.get(urlDict["urlShiba"], parameter),
-                 "rCats" : requests.get(urlDict["urlCats"], parameter),
-                 "rBirds" : requests.get(urlDict["urlBirds"], parameter)}
+whichAnimalEnum = IntEnum("whichAnimalEnum", "dog cat bird") 
 
 
-#SHIBA
-try:
-    pictureShiba = animalReguestDict["rshiba"].json()
-except json.decoder.JSONDecodeError():
-    print("niepoprawny format")
-else:
-    for dog in pictureShiba:
-       webbrowser.open_new_tab(urlDict["urlShiba"])
+animalChoice = int(input(("""Which animal you choose?:
+      1 - Dog
+      2 - Cat
+      3 - Bird""")))   #Dog Cat Bird        
+      
+howManyPictures =int(input("How many pictures you want to see?: "))
 
-#Cats
-try:
-    pictureCats = animalReguestDict["rCats"].json()
-except json.decoder.JSONDecodeError():
-    print("niepoprawny format")
-else:
-    for dog in pictureCats:
-       webbrowser.open_new_tab(urlDict["urlCats"])
-
-# BIRDS
-try:
-    pictureBirds = animalReguestDict["rBirds"].json()
-except json.decoder.JSONDecodeError():
-    print("niepoprawny format")
-else:
-    for dog in pictureBirds:
-       webbrowser.open_new_tab(urlDict["urlBirds"])
-
-
-
-
+if animalChoice == whichAnimalEnum.dog:
+    next_level(howManyPictures)
+elif animalChoice == whichAnimalEnum.cat:
+    next_level(howManyPictures)
+elif animalChoice == whichAnimalEnum.bird:
+    next_level(howManyPictures)
+    
 
 
